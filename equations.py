95fded7efffe03f571fd4b84488cf6235f699377
Ella Fortenbery <ella.fortenbery@duke.edu>
2025-02-14 15:02:53 -0500

    example setup

--- equations.py
+++ equations.py
@@ -1,3 +1,4 @@
 import initial_numbers as c
 
 #equations: (use c.--- to use variables defined in initial numbers file.
+ironMR = c.bloodflowQ*c.ironC
--- initial_numbers.py
+++ initial_numbers.py
@@ -1,1 +1,6 @@
 #initialize all constants here:
+#put everything in micrograms, dL, and day.
+
+bloodflowQ = 5 #L/min
+bloodflowQ = 14400*bloodflowQ #convert to dL/day
+ironC = 92 #microg/dL
