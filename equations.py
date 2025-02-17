f337189a7ef5116043ab7b882c67b3c8bc00a2fe
Ella Fortenbery <ella.fortenbery@duke.edu>
2025-02-17 14:50:36 -0500

    some numbers

--- equations.py
+++ equations.py
@@ -2,3 +2,15 @@ import initial_numbers as c
 
 #equations: (use c.--- to use variables defined in initial numbers file.
 ironMR = c.bloodflowQ*c.ironC
+
+
+#small intestine:
+
+
+#storage:
+
+
+"""notes:
+-for hepcidin, we can extrapolate a relationship (like assuming linear change and guess constants)
+especially if we can find a experimental data point to extrapolalte from. 
+"""
--- initial_numbers.py
+++ initial_numbers.py
@@ -4,3 +4,14 @@
 bloodflowQ = 5 #L/min
 bloodflowQ = 14400*bloodflowQ #convert to dL/day
 ironC = 92 #microg/dL
+
+#mass rates:
+m1iron = 18 #mg/day
+m2iron = 0.1*m1iron
+m3iron = m1iron-m2iron
+m4iron = None
+m5iron = 0.15&m2iron 
+
+accIron = 250 #200-300 mg
+accIron = accIron*1000 #convert microg
+
