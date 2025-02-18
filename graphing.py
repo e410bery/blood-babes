b0feadb59c5d810998f5898157504d1affda701f
Ella Fortenbery <ella.fortenbery@duke.edu>
2025-02-18 16:54:30 -0500

    work2

--- graphing.py
+++ graphing.py
@@ -4,3 +4,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #graphs:
+
+#total iron in blood over time (6 days)
+
+#hormone variation over menstrual cycle
+
+#blood loss over time during cycle
+
+#time vs iron, estrogen, hepcidin in storage
+
+#time vs iron, prog, estrogen in reproductive
