3842633653d07c4d6094658b26ea05c54c5ee6e0
Nora <nora.taylor@duke.edu>
2025-02-23 13:55:36 -0500

    Numbers ready for graphing

--- initial_numbers.py
+++ initial_numbers.py
@@ -26,29 +26,46 @@ m5hemo = m5ironHemo + m5hemo #mg/day
 m10iron = 16.2 #mg/day
 m6iron = m3iron - m10iron #mg/day
 m6hemo = 272000000 #mg/day
-m7iron = c #mg/day
+m7hemo = c #mg/day
 m8hemo = 11000000 #mg/day
 m9hemo = m6hemo + m8hemo - m3hemo - m5hemo #mg/day
-print(m9hemo)
-accIron = c
+conIron = 25 #mg/day
+accEst = c #accumulation of estrogen in the reproductive system, varies per day
 
 
 #mass rates (anemic):
-am2iron = 0.75*m1iron #mg/day - decreased absorption by 25%
-am2hemo = z
-am3iron = m1iron-am2iron #mg/day
-am3hemo = z
-am4iron = 0.15*am2iron #mg/day
-am4hemo = z
-am5iron = 0.85*am2iron #mg/day
-am6iron = z
-am6hemo = 1200 #mg/L (need this out of a concentration and into a rate)
-am7iron = z
-am8iron = z 
-accairon = z
-aconIron = z
-agenHemo = z
+anem_m2iron = 0.075*m1iron #mg/day - decreased absorption by 25%
+anem_m2hemo = 0.075*m1hemo #mg/day - decreased absorption by 25%
+anem_m3iron = 0.925*m1iron #mg/day
+anem_m3hemo = 0.925*m1hemo #mg/day
+anem_m4ironHemo = 0.15*anem_m2iron #mg/day, this is actually hemoglobin since it's coming out of storage, but I wanted to differentiate it
+anem_m4hemo = 0.15*anem_m2hemo #mg/day
+anem_m4hemo = anem_m4ironHemo + anem_m4hemo #mg/day
+anem_m5ironHemo = 0.85*anem_m2iron #mg/day, this is actually hemoglobin since it's coming out of storage, but I wanted to differentiate it
+anem_m5hemo = 0.85*anem_m2hemo #mg/day
+anem_m5hemo = anem_m5ironHemo + anem_m5hemo #mg/day
+anem_m10iron = 16.2 #mg/day
+anem_m6iron = anem_m3iron - anem_m10iron #mg/day
+anem_m6hemo = 272000000 #mg/day
+anem_m7hemo = c #mg/day
+anem_m8hemo = 11000000 #mg/day
+anem_m9hemo = anem_m6hemo + anem_m8hemo - anem_m3hemo - anem_m5hemo #mg/day
+anem_conIron = 25 #mg/day
+anem_accEst = c #accumulation of estrogen in the reproductive system, varies per day
 
-#relationships between hepcidin/iron/estrogen
-hep = z
-est = -0.891*hep + 4.47 #estrogen concentration in ng/mL, calculations done to find equation
+#relationships between hepcidin/iron/estrogen (normal)
+hepIron = z #this should be a formula, undetermined currently
+hepEst = 5.02 + accEst/-0.891
+
+#relationships between hepcidin/iron/estrogen (anemic)
+anem_hepIron = z #this should be a formula, undetermined currently
+anem_hepEst = 5.02 + anem_accEst/-0.891
+
+#normal values that are most relevant to graphing
+accIronStorage = c #calculate this based on the varying amounts of hepcidin resulting from the varying amounts of estrogen (accEst) and varying amounts of iron leaving through menstruation (m7hemo)
+accRep = m4hemo + m8hemo - m7hemo - m9hemo #this value will change based on daily variations of m7hemo
+
+
+#anemic values that are most relevant to graphing
+anem_accIronStorage = c #calculate this based on the varying amounts of hepcidin resulting from the varying amounts of estrogen (anem_accEst) and varying amounts of iron leaving through menstruation (anem_m7hemo)
+anem_accRep = anem_m4hemo + anem_m8hemo - anem_m7hemo - anem_m9hemo #this value will change based on daily variations of anem_m7hemo
