c17e397e1b7b39e9795fd51b90cbaf9947fef103
Nora <nora.taylor@duke.edu>
2025-02-23 13:54:45 -0500

    Lots of new numbers!

--- initial_numbers.py
+++ initial_numbers.py
@@ -2,8 +2,9 @@
 #put everything in micrograms, dL, and day.
 
 z = 1 #placeholder
-c = 1 #value that changes based on day in the cycle
+c = 1 #placeholder to signal a value that changes based on day in the cycle
 m7ironBase = 0.145 #baseline value for m7iron (the actual value changes per day)
+accIronBase = 250*1000 #mg/day, baseline value for accIron
 
 bloodflowQ = 5 #L/min
 bloodflowQ = 14400*bloodflowQ #convert to dL/day
@@ -11,22 +12,25 @@ ironC = 92 #microg/dL
 
 #mass rates (normal):
 m1iron = 18 #mg/day
-m1hemo = z
+m1hemo = 126000000 #mg/day
 m2iron = 0.1*m1iron #mg/day
-m2hemo = 13.3 #g/L (need this out of a concentration and into a rate)
-m3iron = m1iron-m2iron #mg/day
-m3hemo = 13.4 #g/L (need this out of a concentration and into a rate)
-m4iron = 0.15*m2iron #mg/day
-m4hemo = 13.4 #g/L (need this out of a concentration and into a rate)
-m5iron = 0.85*m2iron #mg/day
-m6iron = z
-m6hemo = 6000 #mg/day
-m7iron = 0.145 #mg/day
-m8iron = 54.4*(60*24) #mL/min --> mL/day (**need this in mg)
+m2hemo = 0.1*m1hemo #mg/day
+m3iron = 0.9*m1iron #mg/day
+m3hemo = 0.9*m1hemo #mg/day
+m4ironHemo = 0.15*m2iron #mg/day, this is actually hemoglobin since it's coming out of storage, but I wanted to differentiate it
+m4hemo = 0.15*m2hemo #mg/day
+m4hemo = m4ironHemo + m4hemo #mg/day
+m5ironHemo = 0.85*m2iron #mg/day, this is actually hemoglobin since it's coming out of storage, but I wanted to differentiate it
+m5hemo = 0.85*m2hemo #mg/day
+m5hemo = m5ironHemo + m5hemo #mg/day
 m10iron = 16.2 #mg/day
-accIron = 250*1000 #mg/day
-conIron = 25 #mg/day
-genHemo = 25 #mg/day
+m6iron = m3iron - m10iron #mg/day
+m6hemo = 272000000 #mg/day
+m7iron = c #mg/day
+m8hemo = 11000000 #mg/day
+m9hemo = m6hemo + m8hemo - m3hemo - m5hemo #mg/day
+print(m9hemo)
+accIron = c
 
 
 #mass rates (anemic):
