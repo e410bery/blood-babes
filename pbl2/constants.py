f6d07e2be1e76f8ef1b30a622e3baa5eceefefab
Ella Fortenbery <ella.fortenbery@duke.edu>
2025-03-31 13:54:23 -0400

    newStuff

---
+++ pbl2/constants.py
@@ -0,0 +1,12 @@
+
+#interaction state control variable:
+syrup = False;
+
+c = 1 #placeholder
+if (syrup is True):
+    sert = 0;
+else:
+    sert = c;
+
+
+sertKm = 0.3 #.2-.5 mM, taking S back into prenueron
--- pbl2/equations.py
+++ pbl2/equations.py
@@ -1,1 +1,16 @@
 import constants as c
+
+
+
+#enzyme reactions:
+def competitive_inhibition(sub, vmax, km, inhibitor, ki):
+    return (vmax * sub) / (km * (1 + inhibitor / ki) + sub)
+
+def uncompetitive_inhibition(sub, vmax, km, inhibitor, ki):
+    return (vmax * sub) / (km + sub * (1 + inhibitor / ki))
+
+def noncompetitive_inhibition(sub, vmax, km, inhibitor, ki):
+    return (vmax * sub) / ((km + sub) * (1 + inhibitor / ki))
+
+def mixed_inhibition(sub, vmax, km, inhibitor, ki, ki_prime):
+    return (vmax * sub) / ((km * (1 + inhibitor / ki)) + sub * (1 + inhibitor / ki_prime))
